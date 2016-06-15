59576bcf773bba1ca9afc64694b833432b922a32
Sergey Pepyakin <s.pepyakin@gmail.com>
2016-06-16 00:40:12 +0300

    Add unread notification counter.

--- app/src/main/java/me/pepyakin/her/NotificationController.java
+++ app/src/main/java/me/pepyakin/her/NotificationController.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v4.app.NotificationCompat;
 
@@ -24,7 +25,10 @@ public class NotificationController {
         IntentFilter filter = new IntentFilter(NOTIFICATION_ABOUT_TO_SHOW);
         activity.registerReceiver(swallower, filter);
 
-        getNotificationManager(activity).cancel(NEW_MESSAGE_NOTIF_ID);
+        UnreadNotificationCounter.get(activity)
+                .resetUnreadNotificationCounter();
+        NotificationHandler.get(activity)
+                .hideNotification();
     }
 
     public static void chatActivityStopped(Activity activity) {
@@ -56,33 +60,93 @@ public class NotificationController {
                 null);
     }
 
-    private static void displayNotification(Context context, String message) {
-        Intent intent = MainActivity.buildIntent(context);
-        PendingIntent mainActivityPi = PendingIntent.getActivity(context, 1, intent, 0);
-
-        // TODO: i18n
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setOnlyAlertOnce(true)
-                .setDefaults(NotificationCompat.DEFAULT_ALL)
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentIntent(mainActivityPi)
-                .setContentTitle("New message!")
-                .setTicker(message)
-                .setContentText(message);
-
-        builder.setVisibility(NotificationCompat.VISIBILITY_PRIVATE);
-        builder.setCategory(NotificationCompat.CATEGORY_MESSAGE);
-
-        // Because of real-time chat.
-        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
-
-        NotificationManager nm = getNotificationManager(context);
-        nm.notify(NEW_MESSAGE_NOTIF_ID, builder.build());
+    private static void displayNotification(
+            Context context,
+            @NonNull String message
+    ) {
+        int unreadNotificationCount = UnreadNotificationCounter.get(context)
+                .getAndIncrementUnreadNotificationCount();
+        NotificationHandler.get(context)
+                .displayNotification(message, unreadNotificationCount);
     }
 
-    private static NotificationManager getNotificationManager(Context context) {
-        return (NotificationManager) context.getSystemService(
-                Context.NOTIFICATION_SERVICE);
+    static class NotificationHandler {
+        private final NotificationManager nm;
+        private final Context context;
+
+        NotificationHandler(Context context, NotificationManager nm) {
+            this.nm = nm;
+            this.context = context;
+        }
+
+        public static NotificationHandler get(Context context) {
+            return new NotificationHandler(
+                    context, getNotificationManager(context));
+        }
+
+        public void displayNotification(String message, int totalCount) {
+            Intent intent = MainActivity.buildIntent(context);
+            PendingIntent mainActivityPi = PendingIntent.getActivity(context, 1, intent, 0);
+
+            // TODO: Consider to use inbox style
+            // TODO: i18n
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                    .setOnlyAlertOnce(true)
+                    .setDefaults(NotificationCompat.DEFAULT_ALL)
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(mainActivityPi)
+                    .setContentTitle(totalCount + " new messages!")
+                    .setTicker(message)
+                    .setContentText(message);
+
+            builder.setVisibility(NotificationCompat.VISIBILITY_PRIVATE);
+            builder.setCategory(NotificationCompat.CATEGORY_MESSAGE);
+
+            // Because of real-time chat.
+            builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+
+            nm.notify(NEW_MESSAGE_NOTIF_ID, builder.build());
+        }
+
+        void hideNotification() {
+            nm.cancel(NEW_MESSAGE_NOTIF_ID);
+        }
+
+        private static NotificationManager getNotificationManager(Context context) {
+            return (NotificationManager) context.getSystemService(
+                    Context.NOTIFICATION_SERVICE);
+        }
+    }
+
+    private static final class UnreadNotificationCounter {
+
+        private final SharedPreferences prefs;
+
+        UnreadNotificationCounter(SharedPreferences prefs) {
+            this.prefs = prefs;
+        }
+
+        static UnreadNotificationCounter get(Context context) {
+            return new UnreadNotificationCounter(getNotificationPrefs(context));
+        }
+
+        private static SharedPreferences getNotificationPrefs(Context context) {
+            return context.getSharedPreferences("notifications",
+                    Context.MODE_PRIVATE);
+        }
+
+        public int getAndIncrementUnreadNotificationCount() {
+            int unread = prefs.getInt("unread", 0);
+            int newUnread = unread + 1;
+            prefs.edit()
+                    .putInt("unread", newUnread)
+                    .apply();
+            return newUnread;
+        }
+
+        public void resetUnreadNotificationCounter() {
+            prefs.edit().clear().apply();
+        }
     }
 
     static final class NotificationSwallower extends BroadcastReceiver {
